110d2c9cbdb93392773b0382b031f6dce7e2c154
mohamed-rafik-07 <mohamedrafik1624@gmail.com>
2026-03-17 15:18:48 +0530

    Delete ai_cloud_portfolio_bulder directory

--- ai_cloud_portfolio_bulder/cleanup.py
+++
@@ -1,6 +0,0 @@
-with open('app.py', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-# Truncate at line 415 (the expected end of the file)
-with open('app.py', 'w', encoding='utf-8') as f:
-    f.writelines(lines[:415])
